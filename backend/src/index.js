95476273b366953bf62dcf0eebedd3222cb71634
Veeru123456778 <130406704+Veeru123456778@users.noreply.github.com>
2024-06-03 22:29:18 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,12 @@ const limiter = rateLimit({
   message: "Too many requests, please try again later.",
 });
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://postify-web-app-frontend.vercel.app',
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.static("public/temp"));
 app.use(limiter);
