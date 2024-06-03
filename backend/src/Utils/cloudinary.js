ab831c8c86caad3c1f1d279d061820b1fad15b54
Veeru123456778 <130406704+Veeru123456778@users.noreply.github.com>
2024-06-04 01:00:44 +0530

    Update cloudinary.js

--- backend/src/Utils/cloudinary.js
+++ backend/src/Utils/cloudinary.js
@@ -1,4 +1,37 @@
-import { v2 as cloudinary } from "cloudinary";
+// import { v2 as cloudinary } from "cloudinary";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const uploadOnCloud = async (absoluteFilePath) => {
+//   console.log(absoluteFilePath);
+
+//   try {
+//     if (!absoluteFilePath) {
+//       return null;
+//     }
+//     const response = await cloudinary.uploader.upload(absoluteFilePath, {
+//       resource_type: "auto",
+//     });
+
+//     console.log("File has been uploaded successfully", response.url);
+//     return response;
+//   } catch (error) {
+//     fs.unlinkSync(absoluteFilePath);
+//     return null;
+//   }
+// };
+
+// export { uploadOnCloud };
+
+// Utils/cloudinary.js
+
+import { v2 as cloudinary } from 'cloudinary';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,24 +41,16 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloud = async (absoluteFilePath) => {
-  console.log(absoluteFilePath);
-
-  try {
-    if (!absoluteFilePath) {
-      return null;
-    }
-    const response = await cloudinary.uploader.upload(absoluteFilePath, {
-      resource_type: "auto",
+export const uploadOnCloud = (filePath) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(filePath, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
     });
-
-    console.log("File has been uploaded successfully", response.url);
-    return response;
-  } catch (error) {
-    fs.unlinkSync(absoluteFilePath);
-    return null;
-  }
+  });
 };
 
-export { uploadOnCloud };
 
