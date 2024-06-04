7cfd305efe75011219d9b7bcdaa16096771a3b1a
Veeru123456778 <130406704+Veeru123456778@users.noreply.github.com>
2024-06-04 16:17:06 +0530

    Update cloudinary.js

--- backend/src/Utils/cloudinary.js
+++ backend/src/Utils/cloudinary.js
@@ -25,30 +25,3 @@ export const uploadOnCloud = (filePath) => {
 export { uploadOnCloud };
 
 
-
-// import { v2 as cloudinary } from 'cloudinary';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// export const uploadOnCloud = (filePath) => {
-//   return new Promise((resolve, reject) => {
-//     cloudinary.uploader.upload(filePath, (error, result) => {
-//       if (error) {
-//         reject(error);
-//       } else {
-//         resolve(result);
-//       }
-//     });
-//   });
-// };
-
-
-
->>>>>>> b93f0a9fbce4724d9a712f3a7f04a5140ff5b54d
