d0a6406308760ae9c5635209b0e8e3ec5cd2176d
vineet073 <vkiit11th@gmail.com>
2024-06-05 00:01:43 +0530

    fix:cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,29 +3,26 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import { connectDB } from "./config/Db.js";
 import cors from "cors";
-import rateLimit from "express-rate-limit";
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//allowing 100 requests per minute
-
-const limiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 100,
-  message: "Too many requests, please try again later.",
-});
 
 
-app.use(
-	cors()
+index.use(
+	cors({
+		origin: '*',
+		methods: '*',
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+	  })
 )
 
 app.use(express.json());
 app.use(express.static("public"));
-app.use(limiter);
 
 app.use("/api/user", userRouter);
 
