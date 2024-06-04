783d07d5aed05be7e6b7ba5706e92244ba3cd146
Veeru123456778 <130406704+Veeru123456778@users.noreply.github.com>
2024-06-05 01:58:32 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,14 +12,25 @@ const port = process.env.PORT || 3000;
 
 
 
-app.use(
-	cors({
-		origin: '*',
-		methods: '*',
-		allowedHeaders: ['Content-Type', 'Authorization'],
-		credentials: true,
-	  })
-)
+// app.use(
+// 	cors({
+// 		origin: '*',
+// 		methods: '*',
+// 		allowedHeaders: ['Content-Type', 'Authorization'],
+// 		credentials: true,
+// 	  })
+// )
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust as needed for your scenario
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,token');
+  if (req.method === 'OPTIONS') {
+    // Preflight request
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.static("public"));
