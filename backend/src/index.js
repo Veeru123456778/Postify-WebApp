d5c4e03227d2f4b05c9797965d0d3fdda991ce6e
Veeru123456778 <130406704+Veeru123456778@users.noreply.github.com>
2024-06-05 11:10:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,36 +9,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+connectDB();
 
-
-
-// app.use(
-// 	cors({
-// 		origin: '*',
-// 		methods: '*',
-// 		allowedHeaders: ['Content-Type', 'Authorization'],
-// 		credentials: true,
-// 	  })
-// )
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust as needed for your scenario
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,token');
-  if (req.method === 'OPTIONS') {
-    // Preflight request
-    return res.status(200).end();
-  }
-  next();
-});
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/user", userRouter);
-
-
-connectDB();
 
 app.get("/", (req, res) => {
   res.send("API IS WORKING");
